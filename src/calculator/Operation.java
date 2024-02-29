76074b9404528e3cffbff8a4d930d65e6bf48879
Tanvirnci <tanvirhasanuk849@gmail.com>
2024-02-29 11:19:38 +0000

    first commit

---
+++ src/calculator/Operation.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ * @author EThornbury
+ * /
+ * public class Operations {
+     private double num1, num2,num3, results;
+     * 
+     * public Operations () {//default constructor
+     * num1 =num2 =num3 = results =0;
+     * }
+     * 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package calculator;
+
+/**
+ *
+ * @author tanvi
+ */
+public class Operation {
+    
+    private double num1, num2,num3, results;
+      
+    public Operation () {  //default constructor
+        num1 =num2 =num3 = results =0;
+    }
+    
+    
+      
+    
+    
+    
+    
+}
